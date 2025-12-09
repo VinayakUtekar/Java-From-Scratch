f47d59e316555e25cb9c449cc63c6a93bbaa0791
VinayakUtekar <vinayakvutekar15@gmail.com>
2025-12-09 21:41:53 +0530

    Print Statement

--- Basic/PrintStatements/Print7.java
+++ Basic/PrintStatements/Print7.java
@@ -2,6 +2,11 @@ package Basic.PrintStatements;
  
 public class Print7 {
     public static void main(String [] args){
-        System.out.printf("Hello World");
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = a / b;
+        System.out.println("The result of " + a + " divided by " + b + " is " + c);
+        System.out.printf("The result of %.2f divided by %.2f is %.2f", a, b, c);
+        System.out.format("The result of %.2f divided by %.2f is %.2f", a, b, c);
     }
 }
