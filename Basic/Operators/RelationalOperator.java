34672134d4f38cc02e2c10cd38ad4ced1b785b0b
VinayakUtekar <vinayakvutekar15@gmail.com>
2025-12-13 12:25:14 +0530

    Some More Operators

---
+++ Basic/Operators/RelationalOperator.java
@@ -0,0 +1,26 @@
+package Basic.Operators;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Greater than
+        System.out.println("a > b: " + (a > b)); // false
+
+        // Less than
+        System.out.println("a < b: " + (a < b)); // true
+
+        // Greater than or equal to
+        System.out.println("a >= b: " + (a >= b)); // false
+
+        // Less than or equal to
+        System.out.println("a <= b: " + (a <= b)); // true
+
+        // Equal to
+        System.out.println("a == b: " + (a == b)); // false
+
+        // Not equal to
+        System.out.println("a != b: " + (a != b)); // true
+    }
+}
